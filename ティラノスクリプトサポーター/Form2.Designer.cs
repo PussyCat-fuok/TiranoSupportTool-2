4827a9605ab4f6b8ad98a1cf4d8fc91799f516d9
derumin777 <rikubon.0724@gmail.com>
2020-10-21 18:54:54 +0900

    あーあ

--- ティラノスクリプトサポーター/Form2.Designer.cs
+++ ティラノスクリプトサポーター/Form2.Designer.cs
@@ -33,13 +33,14 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.Location = new System.Drawing.Point(12, 117);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1043, 433);
+            this.panel1.Size = new System.Drawing.Size(1043, 388);
             this.panel1.TabIndex = 0;
             // 
             // button1
@@ -81,11 +82,21 @@
             this.button4.Text = "戻る";
             this.button4.UseVisualStyleBackColor = true;
             // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(980, 527);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1067, 562);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -104,5 +115,6 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
     }
 }
